b683a41b9abf669b47299984c42dcf781d64d12f
TALIG <talig@amdocs.com>
2016-12-19 11:40:39 +0200

    item version public datatypes

--- tsuzammen-commons-datatypes/tsuzammen-commons-datatypes-api/src/main/java/org/amdocs/tsuzammen/commons/datatypes/item/Item.java
+++ tsuzammen-commons-datatypes/tsuzammen-commons-datatypes-api/src/main/java/org/amdocs/tsuzammen/commons/datatypes/item/Item.java
@@ -1,13 +1,32 @@
+/*
+ * Copyright © 2016 Amdocs Software Systems Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.amdocs.tsuzammen.commons.datatypes.item;
 
-import java.util.List;
-
-/**
- * Created by TALIG on 11/29/2016.
- */
 public class Item {
+  private String id;
   private Info info;
-  private List<ItemVersion> versions;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 
   public Info getInfo() {
     return info;
@@ -17,11 +36,4 @@ public class Item {
     this.info = info;
   }
 
-  public List<ItemVersion> getVersions() {
-    return versions;
-  }
-
-  public void setVersions(List<ItemVersion> versions) {
-    this.versions = versions;
-  }
 }
--- tsuzammen-commons-datatypes/tsuzammen-commons-datatypes-api/src/main/java/org/amdocs/tsuzammen/commons/datatypes/item/ItemVersion.java
+++ tsuzammen-commons-datatypes/tsuzammen-commons-datatypes-api/src/main/java/org/amdocs/tsuzammen/commons/datatypes/item/ItemVersion.java
@@ -1,13 +1,45 @@
+/*
+ * Copyright © 2016 Amdocs Software Systems Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.amdocs.tsuzammen.commons.datatypes.item;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ItemVersion {
+  private String id;
+  private String baseId;
   private Info info;
-  private List<Relation> relations;
-  private Map<String, Content> contents = new HashMap<>();
+  private List<Relation> relations = new ArrayList<>();
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getBaseId() {
+    return baseId;
+  }
+
+  public void setBaseId(String baseId) {
+    this.baseId = baseId;
+  }
 
   public Info getInfo() {
     return info;
@@ -21,17 +53,7 @@ public class ItemVersion {
     return relations;
   }
 
-  public void setRelations(
-      List<Relation> relations) {
+  public void setRelations(List<Relation> relations) {
     this.relations = relations;
-  }
-
-  public Map<String, Content> getContents() {
-    return contents;
-  }
-
-  public void setContents(
-      Map<String, Content> contents) {
-    this.contents = contents;
   }
 }
