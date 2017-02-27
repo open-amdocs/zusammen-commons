cf7123380abc74be4ca2c7a6720d5ed6c39d6bba
Shalom Berman <SHALOMB@Amdocs.com>
2017-02-27 19:06:12 +0200

    add logging

---
+++ zusammen-commons-log/src/main/java/org/amdocs/zusammen/commons/log/ZusammenLogger.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2016 Amdocs Software Systems Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.amdocs.zusammen.commons.log;
+
+
+public interface ZusammenLogger {
+  public void debug(String message);
+  public void debug(String format,Object ... args);
+  public void debug(String msg,Throwable t);
+  public void error(String message);
+  public void error(String format,Object ... args);
+  public void error(String msg,Throwable t);
+  public void info(String message);
+  public void info(String format,Object ... args);
+  public void info(String msg,Throwable t);
+  public void trace(String message);
+  public void trace(String format,Object ... args);
+  public void trace(String msg,Throwable t);
+  public void warn(String message);
+  public void warn(String format,Object ... args);
+  public void warn(String msg,Throwable t);
+
+
+
+}
---
+++ zusammen-commons-log/src/main/java/org/amdocs/zusammen/commons/log/impl/ZusammenSLF4JLoggerFactoryImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2016 Amdocs Software Systems Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.amdocs.zusammen.commons.log.impl;
+
+import org.amdocs.zusammen.commons.log.ZusammenLogger;
+import org.amdocs.zusammen.commons.log.ZusammenLoggerFactory;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ZusammenSLF4JLoggerFactoryImpl extends ZusammenLoggerFactory {
+
+  private boolean initialized = false;
+  private Map<String, ZusammenLogger> loggerMap = new HashMap();
+
+  public void init() {
+    initialized = true;
+
+  }
+
+  @Override
+  protected ZusammenLogger createInterface(String key) {
+    ZusammenLogger zusammenLogger;
+    synchronized (this) {
+
+      if (initialized) {
+        zusammenLogger = loggerMap.get(key);
+        if (zusammenLogger == null) {
+          zusammenLogger = new ZusammenSLF4JLoggerImpl(LoggerFactory.getLogger(this.getClass()));
+        }
+      } else {
+        throw new RuntimeException("Logget must be initialized before executing logger. Run " +
+            "method init()");
+      }
+    }
+    return zusammenLogger;
+  }
+}
